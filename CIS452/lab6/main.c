8708300db5d36d150e8d9a687e688ec2a2b7dd5c
Thomas Bailey <baileytb7@gmail.com>
2019-02-28 00:09:43 -0500

    Lab 6 uploaded

---
+++ CIS452/lab6/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/wait.h> 
+#include <sys/ipc.h> 
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/sem.h>
+
+#define SIZE 16
+
+int main (int argc, char** argv) 
+{ 
+   int status; 
+   long int i, loop, temp, *shmPtr; 
+   int shmId; 
+   pid_t pid;
+
+   struct sembuf waits;
+   struct sembuf signal;
+   waits.sem_op = -1;
+   waits.sem_flg = SEM_UNDO;
+   waits.sem_num = 0;
+   signal.sem_op = 1;
+   signal.sem_num = 0;
+   signal.sem_flg = SEM_UNDO;
+
+   loop = atol(argv[1]);
+      
+   if ((shmId = shmget (IPC_PRIVATE, SIZE, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) {
+      perror ("i can't get no..\n"); 
+      exit (1); 
+   } 
+   if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) { 
+      perror ("can't attach\n"); 
+      exit (1); 
+   }
+
+   shmPtr[0] = 0; 
+   shmPtr[1] = 1;
+
+   // create & initialize semaphore
+   int semId;
+   semId = semget(IPC_PRIVATE, 1, 00600);
+   if (semctl(semId, 0, SETVAL, 1) < 0){
+     perror("semaphore problem 2");
+     exit(1);
+   }
+   
+   if (!(pid = fork())) { 
+      for (i=0; i<loop; i++) {
+	semop(semId, &waits, 1);
+	// critical section
+	temp = shmPtr[0];
+	shmPtr[0] = shmPtr[1];
+	shmPtr[1] = temp;
+	// end critical section
+	semop(semId, &signal, 1);
+      } 
+      if (shmdt (shmPtr) < 0) { 
+         perror ("just can't let go\n"); 
+         exit (1); 
+      } 
+      exit(0); 
+   } 
+   else 
+      for (i=0; i<loop; i++) {
+	semop(semId, &waits, 1);
+	// critical section
+	temp = shmPtr[1];
+	shmPtr[1] = shmPtr[0];
+	shmPtr[0] = temp;
+	// end critical section
+	semop(semId, &signal, 1);
+      }
+   wait (&status); 
+   printf ("values: %li\t%li\n", shmPtr[0], shmPtr[1]);
+
+   if(semctl(semId, 0, IPC_RMID) < 0){
+      perror("Removing semctl");
+      exit(1);
+   }
+   
+   if (shmdt (shmPtr) < 0) { 
+      perror ("just can't let go\n"); 
+      exit (1); 
+   } 
+   if (shmctl (shmId, IPC_RMID, 0) < 0) { 
+      perror ("can't deallocate\n"); 
+      exit(1); 
+   }
+
+   return 0; 
+} 
